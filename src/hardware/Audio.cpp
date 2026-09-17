2141bdb0cbe03317c8089555956530a9a9082d1c
Roman Schulte <info@roman-schulte.de>
2026-09-17 21:36:14 +0000

    Simplify audio api

--- src/hardware/Audio.cpp
+++ src/hardware/Audio.cpp
@@ -1,78 +1,15 @@
 #include "Audio.h"
 
-
-AudioChannel::AudioChannel(
-  OutputMixer<int16_t>& mixer,
-  uint8_t mixerIndex,
-  const AudioInfo& info,
-  int16_t volume
-)
-  : _info(info),
-    _sine(volume),
-    _source(_sine),
-    _copier() {
-
-  _copier.begin(
-    mixer[mixerIndex],
-    _source
-  );
-}
-
-
-void AudioChannel::setup() {
-  _source.begin(_info);
-
-  _sine.begin(
-    _info,
-    440.0f
-  );
-}
-
-
-void AudioChannel::tone(float frequency) {
-  _sine.setFrequency(frequency);
-  _playing = true;
-}
-
-
-void AudioChannel::stop() {
-  _playing = false;
-}
-
-
-void AudioChannel::update() {
-  if (!_playing) {
-    return;
-  }
-
-  _copier.copy();
-}
-
-
-// --------------------------------------------------
-
-
 Audio::Audio(const AudioConfig& config)
   : _config(config),
     _info(44100, 2, 16),
-    _mixer(_output, 2, DefaultAllocatorRAM),
-    _music(
-      _mixer,
-      0,
-      _info,
-      config.volume
-    ),
-    _sfx(
-      _mixer,
-      1,
-      _info,
-      config.volume
-    ) {}
-
+    _sine(config.volume),
+    _source(_sine),
+    _copier(_output, _source) {
+}
 
 void Audio::setup() {
-  auto config =
-    _output.defaultConfig(TX_MODE);
+  auto config = _output.defaultConfig(TX_MODE);
 
   config.copyFrom(_info);
 
@@ -82,24 +19,26 @@ void Audio::setup() {
 
   _output.begin(config);
 
-  _mixer.begin();
-
-  _music.setup();
-  _sfx.setup();
+  _sine.begin(
+    _info,
+    440.0f
+  );
 }
-
 
 void Audio::update() {
-  _music.update();
-  _sfx.update();
+  if (!_playing) {
+    return;
+  }
+
+  _copier.copy();
 }
 
-
-AudioChannel& Audio::music() {
-  return _music;
+void Audio::tone(float frequency) {
+  _sine.setFrequency(frequency);
+  _playing = true;
 }
 
-
-AudioChannel& Audio::sfx() {
-  return _sfx;
+void Audio::stop() {
+  _sine.setFrequency(0);
+  _playing = false;
 }
--- src/hardware/Audio.h
+++ src/hardware/Audio.h
@@ -7,35 +7,9 @@ struct AudioConfig {
   uint8_t bclkPin;
   uint8_t lrcPin;
   uint8_t doutPin;
+
   int16_t volume = 3000;
 };
-
-class AudioChannel {
-public:
-  AudioChannel(
-    OutputMixer<int16_t>& mixer,
-    uint8_t mixerIndex,
-    const AudioInfo& info,
-    int16_t volume
-  );
-
-  void setup();
-
-  void tone(float frequency);
-  void stop();
-
-  void update();
-
-private:
-  const AudioInfo& _info;
-
-  SineGenerator<int16_t> _sine;
-  GeneratedSoundStream<int16_t> _source;
-  StreamCopy _copier;
-
-  bool _playing = false;
-};
-
 
 class Audio {
 public:
@@ -44,8 +18,8 @@ public:
   void setup();
   void update();
 
-  AudioChannel& music();
-  AudioChannel& sfx();
+  void tone(float frequency);
+  void stop();
 
 private:
   AudioConfig _config;
@@ -54,8 +28,9 @@ private:
 
   I2SStream _output;
 
-  OutputMixer<int16_t> _mixer;
+  SineGenerator<int16_t> _sine;
+  GeneratedSoundStream<int16_t> _source;
+  StreamCopy _copier;
 
-  AudioChannel _music;
-  AudioChannel _sfx;
+  bool _playing = false;
 };
